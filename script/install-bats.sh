35149445df127644c1f20581100e1cfd4055ae1c
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-09-11 17:25:14 +0200

    ci: install the locked Bats version
    - Install the GitHub tag matching package-lock.json instead of the default branch.
    - Support GITHUB_TOKEN and disable command tracing so curl cannot print the token.

--- script/install-bats.sh
+++ script/install-bats.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
-set -o errexit
-set -o xtrace
+set -eu
 
-git clone --depth 1 https://github.com/bats-core/bats-core
-cd bats-core && ./install.sh "${HOME}/.local" && cd .. && rm -rf bats-core
+bats_npm_package='bats'
+bats_version=$(
+  npm query "#${bats_npm_package}" --package-lock-only \
+    | node -p 'JSON.parse(require("fs").readFileSync(0, "utf8"))[0].version'
+)
+bats_archive_url="https://github.com/bats-core/bats-core/archive/refs/tags/v${bats_version}.tar.gz"
+bats_tmp_dir=$(mktemp -d)
+
+set -- \
+  --fail \
+  --location \
+  --silent \
+  --show-error \
+  --retry 4 \
+  --retry-connrefused
+if [ -n "${GITHUB_TOKEN:-}" ]; then
+  set -- "$@" --header "Authorization: Bearer $GITHUB_TOKEN"
+fi
+
+curl "$@" \
+  --output "${bats_tmp_dir}/bats-core.tar.gz" \
+  "$bats_archive_url"
+tar -xzf "${bats_tmp_dir}/bats-core.tar.gz" -C "$bats_tmp_dir"
+"${bats_tmp_dir}/bats-core-${bats_version}/install.sh" "${HOME}/.local"
+rm -rf "$bats_tmp_dir"
