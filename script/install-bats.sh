cff0985877e4198d6ab8f29952c7ff5fe375385c
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-09-11 14:49:41 +0200

    FORK-ONLY: scope bats dependencies for hardened fork

--- script/install-bats.sh
+++ script/install-bats.sh
@@ -2,5 +2,5 @@
 set -o errexit
 set -o xtrace
 
-git clone --depth 1 https://github.com/bats-core/bats-core
+git clone --depth 1 https://github.com/bats-hardened/bats-core
 cd bats-core && ./install.sh "${HOME}/.local" && cd .. && rm -rf bats-core
